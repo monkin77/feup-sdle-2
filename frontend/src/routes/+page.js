fe30b19486a4728d196a5dc63ef6bd16bd76bfe4
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-11-23 10:30:45 +0000

    Properly deactivating client side rending on a static page

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -7,3 +7,5 @@ const BACKEND_URL = process.env.BACKEND_URL;
 export async function load({ fetch }) {
     return await fetch(BACKEND_URL).then(r => r.json());
 }
+
+export const csr = false; // deactivate client side rendering
