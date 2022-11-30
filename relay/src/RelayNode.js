660c2a2fc9f0e70fcb6b778d2b38ea98ebcba426
Monkin <monkinsmurf@gmail.com>
2022-11-30 19:48:23 +0000

    Relay Nodes have a consistent id

--- relay/src/RelayNode.js
+++ relay/src/RelayNode.js
@@ -4,10 +4,19 @@ import { noise } from "@chainsafe/libp2p-noise";
 import { mplex } from "@libp2p/mplex";
 import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
 import { gossipsub } from "@chainsafe/libp2p-gossipsub";
+import peerIdLib from "peer-id";
+import { createFromJSON } from "@libp2p/peer-id-factory";
+import fs from "fs";
+
+export const createPeerId = async() => {
+    // We needed to use the 'peer-id' lib to export the relay's ids since the new lib had issues exporting to json
+    const peer = await peerIdLib.create({ keyType: "Ed25519", bits: 1024 });
+    console.log(peer.toJSON());
+};
 
 class RelayNode {
     async start() {
-        const nodeOptions = this.buildOptions(process.env.RELAY_IP, process.env.RELAY_PORT);
+        const nodeOptions = await this.buildOptions(process.env.RELAY_IP, process.env.RELAY_PORT, process.env.ID_FILENAME);
         this.node = await createLibp2p(nodeOptions);
 
         await this.node.start();
@@ -22,9 +31,14 @@ class RelayNode {
         console.log("Node has stopped");
     }
 
-    buildOptions(ip, port) {
+    buildOptions = async(ip, port, peerFile) => {
         const address = `/ip4/${ip}/tcp/${port}`;
         console.log(address);
+
+        // Read the relay peer id from the stored file to keep it consistent across restarts
+        const idData = JSON.parse(fs.readFileSync(`./ids/${peerFile}`));
+        const peerId = await createFromJSON(idData);
+
         const nodeOptions = {
             addresses: {
                 listen: [address] // TODO: Check this and consider changing
@@ -33,6 +47,7 @@ class RelayNode {
             connectionEncryption: [noise()],
             streamMuxers: [mplex()],
             pubsub: gossipsub({ allowPublishToZeroPeers: true }),
+            peerId: peerId,
             peerDiscovery: [
                 pubsubPeerDiscovery({
                     interval: 1000
@@ -45,8 +60,9 @@ class RelayNode {
                 }
             },
         };
+
         return nodeOptions;
-    }
+    };
 
     getMultiaddrs() {
         return this.node.getMultiaddrs();
