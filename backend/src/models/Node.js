0f90d6de51f5b98aca0fc6b1172db0de58dec8d3
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-04 20:00:06 +0000

    Allowing an undefined number of boostrap nodes

--- backend/src/models/Node.js
+++ backend/src/models/Node.js
@@ -1,28 +1,25 @@
-import { createLibp2p } from "libp2p";
-import { tcp } from "@libp2p/tcp";
-import { noise } from "@chainsafe/libp2p-noise";
-import { mplex } from "@libp2p/mplex";
-import { bootstrap } from "@libp2p/bootstrap";
-import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
-import { gossipsub } from "@chainsafe/libp2p-gossipsub";
-import { kadDHT } from "@libp2p/kad-dht";
-import { hashPassword, comparePassword, getContent, putContent } from "../utils.js";
+import {createLibp2p} from "libp2p";
+import {tcp} from "@libp2p/tcp";
+import {noise} from "@chainsafe/libp2p-noise";
+import {mplex} from "@libp2p/mplex";
+import {bootstrap} from "@libp2p/bootstrap";
+import {pubsubPeerDiscovery} from "@libp2p/pubsub-peer-discovery";
+import {gossipsub} from "@chainsafe/libp2p-gossipsub";
+import {kadDHT} from "@libp2p/kad-dht";
+import {comparePassword, getContent, hashPassword, putContent} from "../utils.js";
+import {parseBootstrapAddresses} from "../lib/parser.js";
 
 const getNodeOptions = () => {
-    const bootstrap1 = `/ip4/${process.env.BOOTSTRAP_1_IP}/tcp/${process.env.BOOTSTRAP_1_PORT}/p2p/${process.env.BOOTSTRAP_1_ID}`;
-    const bootstrap2 = `/ip4/${process.env.BOOTSTRAP_2_IP}/tcp/${process.env.BOOTSTRAP_2_PORT}/p2p/${process.env.BOOTSTRAP_2_ID}`;
-    const bootstrap3 = `/ip4/${process.env.BOOTSTRAP_3_IP}/tcp/${process.env.BOOTSTRAP_3_PORT}/p2p/${process.env.BOOTSTRAP_3_ID}`;
-    const bootstrapAddresses = [bootstrap1, bootstrap2, bootstrap3];
-    //console.log(bootstrapAddresses);
+    const bootstrapAddresses = parseBootstrapAddresses();
     return {
-    // Libp2p Modules Documentation: https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#modules
+        // Libp2p Modules Documentation: https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#modules
         addresses: {
             listen: ["/ip4/0.0.0.0/tcp/0"], // TODO: Check this and consider changing
         },
         transports: [tcp()],
         connectionEncryption: [noise()],
         streamMuxers: [mplex()],
-        pubsub: gossipsub({ allowPublishToZeroPeers: true, emitSelf: true }),
+        pubsub: gossipsub({allowPublishToZeroPeers: true, emitSelf: true}),
         peerDiscovery: [
             bootstrap({
                 interval: 60e3,
@@ -52,7 +49,7 @@ class Node {
             this.node.info.timeline.push(data);
 
             await putContent(this.node, `/${this.node.info.username}-info`, this.node.info);
-        } else if (evt.detail.topic === `/${this.node.info.username}-follow`) {
+        } else if (evt.detail.topic === "/" + this.node.info.username + "/follow") {
             // If the event is from the Followers Topic, is a Follow Message
             const username = new TextDecoder().decode(evt.detail.data);
             this.node.info.followers.push(username);
@@ -71,8 +68,8 @@ class Node {
     };
 
     /**
-    * Function to reset the info of the node
-    */
+     * Function to reset the info of the node
+     */
     resetInfo() {
         this.node.info = {
             username: "",
@@ -113,19 +110,19 @@ class Node {
     }
 
     /**
-   * Function to register an account.
-   * To do so, it checks if the account already exists on content routing, and if not, it creates it.
-   * @param {*} username
-   * @param {*} password
-   * @returns true if the account was registered successfully, false otherwise.
-   */
+     * Function to register an account.
+     * To do so, it checks if the account already exists on content routing, and if not, it creates it.
+     * @param {*} username
+     * @param {*} password
+     * @returns true if the account was registered successfully, false otherwise.
+     */
     async register(username, password) {
         try {
             await getContent(this.node, `/${username}`);
-            return { success: false, message: "Username already exists" };
+            return {success: false, message: "Username already exists"};
         } catch (err) {
             if (err.code !== "ERR_NOT_FOUND" && err.code !== "ERR_NO_PEERS_IN_ROUTING_TABLE") {
-                return { success: false, message: "Error while registering" };
+                return {success: false, message: "Error while registering"};
             }
 
             // If the account does not exist, create it
@@ -133,20 +130,20 @@ class Node {
             await putContent(this.node, `/${username}`, hashedPassword);
             await putContent(this.node, `/${username}-info`, this.node.info);
 
-            return { success: true, message: "Registration successful" };
+            return {success: true, message: "Registration successful"};
         }
     }
 
     /**
-   * Function to login to an account.
-   * @param {*} username
-   * @param {*} password
-   * @returns success: true if the login was successful, false with error otherwise.
-   */
+     * Function to login to an account.
+     * @param {*} username
+     * @param {*} password
+     * @returns success: true if the login was successful, false with error otherwise.
+     */
     async login(username, password) {
-        
+
         if (this.node.isLoggedIn)
-            return { success: false, message: "Already logged in" };
+            return {success: false, message: "Already logged in"};
 
         try {
             const hashedPass = await getContent(this.node, `/${username}`);
@@ -158,40 +155,40 @@ class Node {
                 this.node.pubsub.subscribe("/" + this.node.info.username + "/follow");
                 this.node.pubsub.subscribe("/" + this.node.info.username + "/unfollow");
 
-                return { success: true, message: "Login successful" };
+                return {success: true, message: "Login successful"};
             } else {
-                return { success: false, message: "Wrong password" };
+                return {success: false, message: "Wrong password"};
             }
         } catch (err) {
             console.log("err: ", err);
-            return { success: false, message: "Username does not exist" };
+            return {success: false, message: "Username does not exist"};
         }
     }
 
     /**
-   * Function to logout of account.
-   * @returns success: true if the login was successful, false with error otherwise.
-   */
+     * Function to logout of account.
+     * @returns success: true if the login was successful, false with error otherwise.
+     */
     async logout() {
         if (!this.node.isLoggedIn)
-            return { success: false, message: "Already logged out" };
+            return {success: false, message: "Already logged out"};
 
         await this.stop();
         await this.start();
-        return { success: true, message: "Logged out successfully" };
+        return {success: true, message: "Logged out successfully"};
     }
 
     /**
-   * Subscribe to a user and set the callback function to be called when a new message is received.
-   * @param {*} username Username of the user to follow
-   */
+     * Subscribe to a user and set the callback function to be called when a new message is received.
+     * @param {*} username Username of the user to follow
+     */
     async follow(username) {
         if (!this.node.isLoggedIn) {
-            return { success: false, message: "You must login" };
+            return {success: false, message: "You must login"};
         } else if (this.node.info.following.includes(username)) {
-            return { success: false, message: "Already following" };
+            return {success: false, message: "Already following"};
         } else if (this.node.info.username === username) {
-            return { success: false, message: "You cannot follow yourself" };
+            return {success: false, message: "You cannot follow yourself"};
         }
 
         try {
@@ -208,25 +205,25 @@ class Node {
             this.node.info.following.push(username);
             await putContent(this.node, `/${username}-info`, this.node.info);
 
-            return { success: true, message: `Follow successful user ${username}` };
+            return {success: true, message: `Follow successful user ${username}`};
         } catch (err) {
-            return { success: false, message: "User does not exist" };
+            return {success: false, message: "User does not exist"};
         }
     }
 
     /**
-   * Unsubscribe from a user.
-   * @param {*} username
-   */
+     * Unsubscribe from a user.
+     * @param {*} username
+     */
     async unfollow(username) {
         if (!this.node.isLoggedIn)
-            return { success: false, message: "You must login" };
+            return {success: false, message: "You must login"};
         else if (!this.node.info.following.includes(username))
-            return { success: false, message: "You are not following this user" };
+            return {success: false, message: "You are not following this user"};
 
         try {
             await getContent(this.node, `/${username}`);
-            
+
             // username exists so we can unfollow it
             this.node.pubsub.unsubscribe(username);
             this.node.info.following.splice(
@@ -241,21 +238,21 @@ class Node {
 
             await putContent(this.node, `/${username}-info`, this.node.info);
 
-            return { success: true, message: `Unfollow successful user ${username}` };
+            return {success: true, message: `Unfollow successful user ${username}`};
         } catch (err) {
-            return { success: false, message: "User does not exist" };
+            return {success: false, message: "User does not exist"};
 
         }
     }
-  
+
     /**
-   * Function to post a message.
-   * @param {*} text Post content message
-   * @returns  success: true if the post was successful, false with error otherwise.
-   */
+     * Function to post a message.
+     * @param {*} text Post content message
+     * @returns  success: true if the post was successful, false with error otherwise.
+     */
     async post(text) {
         if (!this.node.isLoggedIn)
-            return { success: false, message: "You must login" };
+            return {success: false, message: "You must login"};
 
         try {
             const post = {
@@ -274,25 +271,25 @@ class Node {
 
             await putContent(this.node, `/${this.node.info.username}-info`, this.node.info);
 
-            return { success: true, message: "Post successful" };
+            return {success: true, message: "Post successful"};
         } catch (err) {
             console.log("err: ", err);
-            return { success: false, error: "Failed to post" };
+            return {success: false, error: "Failed to post"};
         }
     }
 
     /**
      * Get the followers of a user.
-     * @param {*} username 
+     * @param {*} username
      * @returns success: true with followers if could get the followers, false with error message otherwise.
      */
     async getFollowers(username) {
-        // 
+        //
         try {
             const data = await getContent(this.node, `/${username}-info`);
-            return { success: true, data: data };
+            return {success: true, data: data};
         } catch (err) {
-            return { success: false, error: "User does not exist" };
+            return {success: false, error: "User does not exist"};
         }
     }
 }
