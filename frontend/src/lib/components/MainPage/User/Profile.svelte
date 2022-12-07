7ea5594703a08ce680c7303f9a8a1edb9f4ecefd
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-07 10:31:57 +0000

    Logging out from application

--- frontend/src/lib/components/MainPage/User/Profile.svelte
+++ frontend/src/lib/components/MainPage/User/Profile.svelte
@@ -2,7 +2,23 @@
     import Icon from '@iconify/svelte';
 	import { isLoggedIn } from '../../../stores';
     import avatar from "./avatar.svg";
+    import { PUBLIC_BACKEND_URL } from '$env/static/public';
+
     export let username;
+
+    const logout = async () => {
+        const res = await fetch(PUBLIC_BACKEND_URL + "/auth/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (res.ok) {
+            isLoggedIn.update(() => false);
+        }
+        console.log(res);
+    }
 </script>
 
 <div class="flex justify-between items-center">
@@ -10,7 +26,7 @@
         <img src={avatar} class="rounded-full w-16 h-16" alt="your avatar" />
         <p class="px-3">{username}</p>
     </div>
-    <button class="hover:bg-slate-700 rounded-md h-9 w-9" on:click={() => isLoggedIn.update(() => false)}>
+    <button class="hover:bg-slate-700 rounded-md h-9 w-9" on:click={logout}>
         <Icon class="w-9 h-7" icon="material-symbols:logout" />
     </button>
 </div>
