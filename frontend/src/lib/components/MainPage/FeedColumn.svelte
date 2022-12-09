c060b9a35a68cfa8cf43596bf53bfef69363a77f
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-09 18:38:40 +0000

    Added messages for empty sections

--- frontend/src/lib/components/MainPage/FeedColumn.svelte
+++ frontend/src/lib/components/MainPage/FeedColumn.svelte
@@ -4,8 +4,12 @@
     import Post from "./Posts/Post.svelte";
 </script>
 
-<ColumnLayout>
+<ColumnLayout>       
     <h3 class="text-3xl text-center font-extrabold tracking-wide pb-5">Welcome to Fake Social! 😁</h3>
+
+    {#if $timeline.length === 0}
+        <p class="text-center text-gray-500">Such an empty section... You should totally share what's going on with you!</p>
+    {/if}
 
     <div class="overflow-y-auto px-2">
         {#each $timeline as post}
