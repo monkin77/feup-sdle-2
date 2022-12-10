78a7b4e6279ea3a83b604766b7c3a6586c424078
Henrique Nunes <hrn2001@gmail.com>
2022-12-10 22:06:03 +0000

    Fix from reviews

--- backend/src/lib/peer-content.js
+++ backend/src/lib/peer-content.js
@@ -12,7 +12,7 @@ import { getAllPosts } from "./storage.js";
  * @param {*} key
  * @returns content if success, throws error otherwise
  */
-export const getContent = async(node, key) => {
+export const getContent = async (node, key) => {
     const content = await node.contentRouting.get(
         new TextEncoder().encode(key)
     );
@@ -26,7 +26,7 @@ export const getContent = async(node, key) => {
  * @param {string} key
  * @param {*} value
  */
-export const putContent = async(node, key, value) => {
+export const putContent = async (node, key, value) => {
     try {
         await node.contentRouting.put(
             new TextEncoder().encode(key),
@@ -47,7 +47,7 @@ export const putContent = async(node, key, value) => {
  * @param {string} topic
  * @param {string} message
  */
-export const publishMessage = async(node, topic, message) => {
+export const publishMessage = async (node, topic, message) => {
     await node.pubsub.publish(
         topic,
         new TextEncoder().encode(message)
@@ -61,7 +61,7 @@ export const discoveryTopic = "_peer-discovery._p2p._pubsub";
  * Register the lookup function to be called when a peer wants to get the content.
  * @param {string} key 
  */
-export const provideInfo = async(key) => {
+export const provideInfo = async (key) => {
     const node = peer.node;
     const cid = await createCID(key);
 
@@ -70,7 +70,7 @@ export const provideInfo = async(key) => {
 
     // Node register the retrieve function to be called when a peer request the content
     try {
-        node.fetchService.registerLookupFunction(`/${key}`, async() => {
+        node.fetchService.registerLookupFunction(`/${key}`, async () => {
             const info = await peer.getInfo(key);
             return new TextEncoder().encode(JSON.stringify(info));
         });
@@ -84,7 +84,7 @@ export const provideInfo = async(key) => {
  * The unprovide can't be done since there is no support for it.
  * @param {string} key 
  */
-export const unprovideInfo = async(key) => {
+export const unprovideInfo = async (key) => {
     const node = peer.node;
 
     // TODO: Check if we don't need anything else to unprovide
@@ -114,12 +114,16 @@ export const collectInfo = async (key) => {
     if (foundProviders) {
         console.log(`Found ${providers.length} providers for ${key}: ${providers.map(provider => provider.id)}`);
 
-        // TODO: Check how info will be updated from the providers
         const infos = [];
         for (const provider of providers) {
             try {
                 const infoReq = await node.fetch(provider.id, `/${key}`);
-                infos.push(JSON.parse(new TextDecoder().decode(infoReq)).data);
+                const { error, data } = JSON.parse(new TextDecoder().decode(infoReq));
+                if (!error) {
+                    infos.push(data);
+                } else {
+                    console.log(`Error in provider ${provider.id}: ${error}.`);
+                }
             } catch (err) {
                 console.log(`Error fetching info from provider ${provider.id}: ${err}. Trying next...`);
             }
@@ -139,7 +143,7 @@ export const collectInfo = async (key) => {
  * @param {*} key 
  * @returns List of peers that provide the content excluding itself. If no peers are found, returns an empty list.
  */
-export const getPeerProviders = async(key) => {
+export const getPeerProviders = async (key) => {
     const cid = await createCID(key);
 
     let providers = [];
@@ -160,7 +164,7 @@ export const getPeerProviders = async(key) => {
  * @param {string} content 
  * @returns CID of the content
  */
-const createCID = async(content) => {
+const createCID = async (content) => {
     const bytes = new TextEncoder().encode(content);
     const hash = await sha256.digest(bytes);
     return CID.create(1, 0x55, hash);
@@ -186,12 +190,12 @@ export const findRecommendedUsers = async () => {
     let recommendedUsers = [];
     const following = profile.getFollowing();
     for (const user of following) {
-        const {error, data} = await collectInfo(user);
+        const { error, data } = await collectInfo(user);
         if (error) continue;
         const followingFollowings = data.following;
         recommendedUsers.push(...followingFollowings);
         for (const subuser of followingFollowings) {
-            const {error, data} = await collectInfo(subuser);
+            const { error, data } = await collectInfo(subuser);
             if (error) continue;
             recommendedUsers.push(...data.following);
         }
@@ -214,7 +218,7 @@ const mergeInfos = (infos) => {
      * @property {Dict} following Dictionary with the votes for each following.
      * @property {Dict} posts Dictionary with the votes for each post timestamp.
      */
-    const votation = {
+    const votes = {
         followers: {},
         following: {},
         posts: {},
@@ -222,22 +226,22 @@ const mergeInfos = (infos) => {
 
     for (const info of infos) {
         for (const follower of info.followers) {
-            votation.followers[follower] = (votation.followers[follower] || 0) + 1;
+            votes.followers[follower] = (votes.followers[follower] || 0) + 1;
         }
         for (const following of info.following) {
-            votation.following[following] = (votation.following[following] || 0) + 1;
+            votes.following[following] = (votes.following[following] || 0) + 1;
         }
         for (const post of info.posts) {
-            votation.posts[post.timestamp] = (votation.posts[post.timestamp] || 0) + 1;
+            votes.posts[post.timestamp] = (votes.posts[post.timestamp] || 0) + 1;
             posts[post.timestamp] = post;
         }
     }
 
     // Use the info that has been voted by more than half of the peers
     const info = {
-        followers: Object.entries(votation.followers).filter(key => key[1] >= infos.length/2).map(key => key[0]),
-        following: Object.entries(votation.following).filter(key => key[1] >= infos.length/2).map(key => key[0]),
-        posts: Object.entries(votation.posts).filter(key => key[1] >= infos.length/2).map(key => posts[key[0]]),
+        followers: Object.entries(votes.followers).filter(entry => entry[1] > infos.length / 2).map(entry => entry[0]),
+        following: Object.entries(votes.following).filter(entry => entry[1] > infos.length / 2).map(entry => entry[0]),
+        posts: Object.entries(votes.posts).filter(entry => entry[1] > infos.length / 2).map(entry => posts[entry[0]]),
     };
-    return info;    
+    return info;
 };
